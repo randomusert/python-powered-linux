ce006824372bebe5305739c1648fa330eb5dca74
randomusert <eetupajamaki@gmail.com>
2026-05-04 18:01:30 +0300

    script to have a basic runner without needing to run init directly

---
+++ test.py
@@ -0,0 +1,8 @@
+
+def test():
+    print("This script is runs the init script for the linux distro, it is used for testing purposes only on windows and should not be included in the final product")
+    from src.init import init
+    init.init()
+
+if __name__ == "__main__":
+    test()
