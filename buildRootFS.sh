c7a9a4c43a37642e333b8c0ba52887212ee14cd6
randomusert <eetupajamaki@gmail.com>
2026-09-11 11:38:42 +0300

    Fixes to the build rootfs

--- buildRootFS.sh
+++ buildRootFS.sh
@@ -9,6 +9,19 @@ check_path_to_root() {
     fi
 }
 
+while getopts "r:" opt; do
+    case $opt in
+        r)
+            TINOSPPLE_ROOTFS="$OPTARG"
+            ;;
+        *)
+            echo "Usage: $0 -r <path_to_tinospple_rootfs>"
+            exit 1
+            ;;
+    esac
+done
+
+check_path_to_root()
 find $TINOSPPLE_ROOTFS -print0 | cpio --null -ov --format=newc | gzip -9 > "$HOME/tinospple_initrd.img"
 
 echo "Root filesystem built successfully at $HOME/tinospple_initrd.img"
