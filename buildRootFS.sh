ae233745ef160d0fa481dfeef2325445d9ff59a6
randomusert <eetupajamaki@gmail.com>
2026-09-11 13:05:59 +0300

    fully working build script

--- buildRootFS.sh
+++ buildRootFS.sh
@@ -1,12 +1,15 @@
+#!/bin/sh
 set -e
 set -o pipefail
 
 
-check_path_to_root() {
+build_rootfs() {
     if [ ! -d "$TINOSPPLE_ROOTFS" ]; then
         echo -e "Error: TINOSPPLE_ROOTFS directory does not exist or the variable is not set as a argument. Please do buildRootFS.sh $TINOSPPLE_ROOTFS=/path/to/tinospple_rootfs"
         exit 1
     fi
+
+    find $TINOSPPLE_ROOTFS -print0 | cpio --null -ov --format=newc | gzip -9 > "$HOME/tinospple_initrd.img"
 }
 
 while getopts "r:" opt; do
@@ -21,7 +24,6 @@ while getopts "r:" opt; do
     esac
 done
 
-check_path_to_root()
-find $TINOSPPLE_ROOTFS -print0 | cpio --null -ov --format=newc | gzip -9 > "$HOME/tinospple_initrd.img"
+build_rootfs
 
-echo "Root filesystem built successfully at $HOME/tinospple_initrd.img"
+echo "Root filesystem built successfully at ~/tinospple_initrd.img"
